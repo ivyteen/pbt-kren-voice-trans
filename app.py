53d75671a22d58545bea3cf3ed673c294524c82c
ivyteen <neorafale@gmail.com>
2022-02-09 15:09:31 +0900

    Update app.py

--- app.py
+++ app.py
@@ -17,8 +17,7 @@ def render_vt_page():
 @app.route('/trans', methods=['POST'])
 def translate():
     
-    #미션 : 아래 힌트와 API 예제 코드를 활용하여 한영 동시 통역기를 구현하고, 
-    # 구현내용을 정리하여 발표하세요
+    #미션 : 아래 힌트와 API 예제 코드를 활용하여 한영 동시 통역기를 구현하세요.
     
     #힌트 1 (STT 오디오 입력)
     #file = request.files['file']
@@ -33,11 +32,11 @@ def translate():
     #힌트 4 (번역 API 응답에서 text 추출)
     #papago_text = response.json()['message']['result']['translatedText']
     
-    #힌트 4 (TTS 요청 파라미터)
+    #힌트 5 (TTS 요청 파라미터)
     #https://api.ncloud-docs.com/docs/ai-naver-clovavoice-ttspremium 참고
     #스피커(speaker)는 "clara", 포맷(format)은 "wav"를 사용하세요
     
-    #힌트 5 (TTS 응답 리턴)
+    #힌트 6 (TTS 응답 리턴)
     #return Response(response.content, mimetype="audio/wav")
     
     return
